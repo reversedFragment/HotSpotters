51b1fec3eade0e5b3b4c6d717f931e471f864c2d
Unknown <trevor.adcock@mac.com>
2018-06-28 11:03:37 -0600

    Merge branch 'trevor' into origin/develop

---
+++ HotSpotters/Models/Event.swift
@@ -0,0 +1,49 @@
+//
+//  Event.swift
+//  EventBrite
+//
+//  Created by CELLFiY on 6/27/18.
+//  Copyright © 2018 Matt Schweppe. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+struct Events {
+    
+    let name: String
+    let image: UIImage
+    let description: String
+    let location: CLLocationCoordinate2D
+    let posts: [Post]
+    let category: CategoryType
+    
+    
+}
+
+
+enum CategoryType: String {
+    
+    case Music, Business, Dining, Community, Entertainment, Health, Technology
+    
+    func description() -> String {
+        switch self {
+        case .Music:
+            return "Music"
+        case .Business:
+            return "Business & Professional"
+        case .Dining:
+            return "Food & Drink"
+        case .Community:
+            return "Community & Culture"
+        case .Entertainment:
+            return "Film, Performing Arts & Entertainment"
+        case .Health:
+            return "Health & Wellness"
+        case .Technology:
+            return "Science & Technology"
+            
+        }
+    }
+    
+}
