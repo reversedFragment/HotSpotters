da86052347cbe5f582edda35df52ef4a8f7b44c2
Ben Adams <benadams@me.com>
2018-07-02 22:38:39 -0600

    Putting data on the map let's go

--- HotSpotters/ViewControllers/BSAMapViewDynamicDataPractice.swift
+++ HotSpotters/ViewControllers/BSAMapViewDynamicDataPractice.swift
@@ -8,33 +8,31 @@
 
 import Mapbox
 
-class BSAMapViewDynamicDataPractice: UIViewController, MGLMapViewDelegate {
-
-override func viewDidLoad() {
-    
-    let mapView = MGLMapView(frame: view.bounds)
-    mapView.styleURL = MGLStyle.lightStyleURL
-    mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    
-    mapView.setCenter(CLLocationCoordinate2D(latitude: 44.971, longitude: -93.261), zoomLevel: 10, animated: false)
-    
-    mapView.delegate = self
-    view.addSubview(mapView)
-    
+class BSAMapViewDynamicDataPracticeViewController: UIViewController, MGLMapViewDelegate {
+    override func viewDidLoad() {
+        
+        let mapView = MGLMapView(frame: view.bounds)
+        mapView.styleURL = MGLStyle.lightStyleURL
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 44.971, longitude: -93.261), zoomLevel: 10, animated: false)
+        
+        mapView.delegate = self
+        view.addSubview(mapView)
     }
     
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
         
-        let source = MGLVectorTileSource(identifier: "historical-places", configurationURL: URL(string: "mapbox://examples.5zzwbooj")!)
+        let source = MGLVectorTileSource(identifier: "badams4khd.5kvp0rhs", configurationURL: URL(string: "mapbox://examples.5zzwbooj")!)
         style.addSource(source)
         
         let layer = MGLCircleStyleLayer(identifier: "landmarks", source: source)
+        layer.circleRadius = NSExpression(format: "(2018 - Constructi) / 10")
         layer.sourceLayerIdentifier = "HPC_landmarks-b60kqn"
-        layer.circleRadius = NSExpression(format: "2018 - Constructi / 10")
         layer.circleColor = NSExpression(forConstantValue: #colorLiteral(red: 0.67, green: 0.28, blue: 0.13, alpha: 1))
         layer.circleOpacity = NSExpression(forConstantValue: 0.8)
+        
         style.addLayer(layer)
         
     }
 }
-
