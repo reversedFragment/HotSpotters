4033c046d175527d63161994c2107b9c045bdd03
Unknown <trevor.adcock@mac.com>
2018-07-18 17:45:58 -0600

    Started to add event annotations and debugged annotations disappearing

---
+++ Extensions/UIVIewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIVIewExtension.swift
+//  HotSpotters
+//
+//  Created by Trevor Adcock on 7/18/18.
+//  Copyright © 2018 Ben Adams. All rights reserved.
+//
+
+import Foundation
---
+++ HotSpotters/Models/EventbriteModels/EventAnnotations.swift
@@ -0,0 +1,9 @@
+//
+//  EventAnnotations.swift
+//  HotSpotters
+//
+//  Created by Trevor Adcock on 7/18/18.
+//  Copyright © 2018 Ben Adams. All rights reserved.
+//
+
+import Foundation
